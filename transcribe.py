d17c753e44a75cc37af8a96b2558c06e02f63166
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-09-19 16:51:29 +0530

    Update Codebase

--- transcribe.py
+++ transcribe.py
@@ -28,15 +28,16 @@ firebase_admin.initialize_app(cred, firebaseConfig)
 
 rtdb = db.reference()
 
+
 class video(Thread):
-    def __init__(self, link):
+    def __init__(self, link, meeting_id):
         self.link = link
         Thread.__init__(self)
-        if not os.path.exists('hls/hlsnew'):
-            os.makedirs('hls/hlsnew')
+        if not os.path.exists(f'meetings/{meeting_id}'):
+            os.makedirs(f'meetings/{meeting_id}')
 
-    def run(self):
-        cmd = f'ffmpeg -y -i {self.link} -vn -c:a copy hls/hlsnew/audiotst.wav'
+    def run(self, meeting_id):
+        cmd = f'ffmpeg -y -i {self.link} -vn -c:a copy meetings/{meeting_id}/audiotst.wav'
         os.system(cmd)
 
 
@@ -44,7 +45,7 @@ def transcribe_audio(hrl_link, meeting_id):
     t1 = video(link=hrl_link)
     t1.start()
     while True:
-        if 'audiotst.wav' in os.listdir('hls/hlsnew'):
+        if 'audiotst.wav' in os.listdir(f'meetings/{meeting_id}'):
             break
     count = 0
     trans = {}
@@ -52,20 +53,20 @@ def transcribe_audio(hrl_link, meeting_id):
     time.sleep(40)
     while True:
         start = time.time()
-        cmd = f'ffmpeg -y -i hls/hlsnew/audiotst.wav -ss {count} -t 60 -vn -c:a copy hls/hlsnew/audioclip.wav'
+        cmd = f'ffmpeg -y -i meetings/hlsnew/audiotst.wav -ss {count} -t 60 -vn -c:a copy meetings/{meeting_id}/audioclip.wav'
         os.system(cmd)
 
-        d, sr = librosa.load('hls/hlsnew/audioclip.wav', sr=None)
+        d, sr = librosa.load(f'meetings/{meeting_id}/audioclip.wav', sr=None)
 
         if len(d) == 0:
             break
-        out = MODEL.transcribe('hls/hlsnew/audioclip.wav')['text']
+        out = MODEL.transcribe(f'meetings/{meeting_id}/audioclip.wav')['text']
         print(out)
 
         ind = (count // 60) + 1
         trans[ind] = out
 
-        file = open('hls/hlsnew/transcript1.json', 'w')
+        file = open(f'meetings/{meeting_id}/transcript.json', 'w')
         json.dump(trans, file, ensure_ascii=False)
         file.close()
 
@@ -74,7 +75,7 @@ def transcribe_audio(hrl_link, meeting_id):
         if ind % 3 == 0:
             t3m = ''
 
-            file = open('hls/hlsnew/transcript1.json', 'r')
+            file = open(f'meetings/{meeting_id}/transcript.json', 'r')
             nas = json.load(file)
             file.close()
 
@@ -85,7 +86,7 @@ def transcribe_audio(hrl_link, meeting_id):
 
             summary[ind // 3] = s3m
 
-            file = open('hls/hlsnew/summary.json', 'w')
+            file = open(f'meetings/{meeting_id}/summary.json', 'w')
             json.dump(summary, file, ensure_ascii=False)
             file.close()
 
@@ -99,4 +100,3 @@ def transcribe_audio(hrl_link, meeting_id):
 
         if t > 0:
             time.sleep(60 - t)
-
